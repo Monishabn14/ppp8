bb9e781d72688db3417a06078dd70895ed773d56
Monisha B N <monishabn1406@gmail.com>
2025-06-09 09:26:04 +0530

    first commit

---
+++ ppp8/src/main/java/com/Monishabn14/App.java
@@ -0,0 +1,13 @@
+package com.Monishabn14;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
